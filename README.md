029f15642842b642e33bf20b7177a0d60d5199cd
yonichuu <yoanna.stamenova@gmail.com>
2022-03-13 07:56:00 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Planet Trip
+
+A new Flutter project.
+
+## Getting Started
+
+A Flutter app for walkthrough through the planets of the Sun 
+and background information about them. Connected with the NASA APOD API
+for a daily picture and background information about it.
---
+++ lib/main_page.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:Planet_trip/detail_page.dart';
+import 'package:Planet_trip/swiper_data.dart';
+import 'constants.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: gradientEndColor,
+      body: SafeArea(
+        top: false,
+        child: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/NC9ufI.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          'Planeto',
+                          style: GoogleFonts.poppins(
+                            fontSize: 44.0,
+                            fontWeight: FontWeight.w900,
+                            color: titleTextColor,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 500.0,
+                    padding: const EdgeInsets.only(left: 5.0),
+                    child: Swiper(
+                      // autoplay: true,
+                      itemCount: planets.length,
+                      itemWidth: MediaQuery.of(context).size.width - 2 * 64,
+                      layout: SwiperLayout.STACK,
+                      pagination: SwiperPagination(
+                        builder: DotSwiperPaginationBuilder(
+                          activeSize: 15.0,
+                          space: 8.0,
+                        ),
+                      ),
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder: (context, a, b) => DetailPage(
+                                  planetInfo: planets[index],
+                                ),
+                              ),
+                            );
+                          },
+                          child: Stack(
+                            children: <Widget>[
+                              Column(
+                                children: <Widget>[
+                                  SizedBox(height: 100.0),
+                                  Card(
+                                    elevation: 8.0,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(32),
+                                    ),
+                                    color: Colors.transparent,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(32.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Positioned(
+                                bottom: 50.0,
+                                right: 0,
+                                child: Text(
+                                  planets[index].name.toString(),
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 150.0,
+                                    fontWeight: FontWeight.w900,
+                                    color: primaryTextColor.withOpacity(0.15),
+                                  ),
+                                ),
+                              ),
+                              Hero(
+                                tag: planets[index].name,
+                                child: Image.asset(planets[index].iconImage),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      
+    );
+  }
+}
